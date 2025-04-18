fe2d18a424eae275acec3ed01caa1cab885aa769
José Antonio Márquez <jolejuma.sspa@gmail.com>
2025-04-19 01:33:42 +0200

    1:30

--- 02_balance_modo_nocturno/dark/menu.js
+++
@@ -1,7 +0,0 @@
-const btn_menu = document.querySelector(".btn-menu"),
-    menu_options = document.querySelector(".menu-options");
-
-btn_menu.onclick = () => {
-    menu_options.classList.toggle("active");
-}
-
